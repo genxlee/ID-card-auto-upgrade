b9de49a4646e1b8588fa04bb35fc6439b95df428
Gen Lee <gen97lee@gmail.com>
2017-07-20 22:18:16 +0300

    Add files via upload

---
+++ idupgrade_taskscheduler.ps1
@@ -0,0 +1,17 @@
+﻿# Script: Task Scheduler ID upgrade
+# Author: Gen Lee
+# Date: 03/03/2017
+
+if(Test-Path 'C:\Program Files\Open-EID\ID-updater.exe'){
+    $softver='C:\Program Files\Open-EID\ID-updater.exe'
+}
+else{
+    $softver='C:\Program Files (x86)\Open-EID\ID-updater.exe'
+}
+
+$soft = New-ScheduledTaskAction -Execute $softver -Argument '-autoupdate'
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
+
+Register-ScheduledTask -Action $soft -Trigger $trigger -Settings $settings `
+    -TaskName "ID-updater" -Description "Updates ID card software at start up"
