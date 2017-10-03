eb96715593cbe34a30f83a0a428ac7e61e560da7
Gen Lee <github@g-l.ee>
2017-10-03 11:18:29 +0300

    Version 1.3
    Running script now as SYSTEM account;
    Running script at 11:15 with random delay of 30 mins every 3 days;
    When scheduled task fails it will be restarted for 3 times after one hour.

--- idupgrade_taskscheduler.ps1
+++ idupgrade_taskscheduler.ps1
@@ -1,17 +1,19 @@
-﻿# Script: Task Scheduler ID upgrade
+# Script: Task Scheduler ID upgrade
 # Author: Gen Lee
 # Date: 03/03/2017
-
+# Updated 03/10/2017
+Unregister-ScheduledTask -TaskName "id updater task" -Confirm:$false #Open-EID default scheduler
+Unregister-ScheduledTask -TaskName "ID-updater" -Confirm:$false #Older automated Task Scheduler
 if(Test-Path 'C:\Program Files\Open-EID\ID-updater.exe'){
-    $softver='C:\Program Files\Open-EID\ID-updater.exe'
+    $soft='C:\Program Files\Open-EID\ID-updater.exe'
 }
 else{
-    $softver='C:\Program Files (x86)\Open-EID\ID-updater.exe'
+    $soft='C:\Program Files (x86)\Open-EID\ID-updater.exe'
 }
+$restart_interval = (New-TimeSpan -Minutes 60)
+$action = New-ScheduledTaskAction -Execute $soft -Argument '-autoupdate -autoclose'
+$trigger = New-ScheduledTaskTrigger -Daily -DaysInterval 3 -RandomDelay "00:30" -At "11:15"
+$settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -RestartCount 3 -RestartInterval $restart_interval
 
-$soft = New-ScheduledTaskAction -Execute $softver -Argument '-autoupdate -autoclose'
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
-
-Register-ScheduledTask -Action $soft -Trigger $trigger -Settings $settings `
-    -TaskName "ID-updater" -Description "Updates ID card software at start up"
+Register-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -User "SYSTEM" -RunLevel Highest `
+    -TaskName "ID-updater 1.3" -Description "Updates ID card software"
